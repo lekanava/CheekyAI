f55ee2c94c42e15024bb3ac128aff7bfa0e39d39
lekanava <lekanava8@gmail.com>
2025-03-05 18:43:05 +0900

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,1 +1,16 @@
+import { useDispatch } from "react-redux";
+import { clearMessages } from "../store/messageSlice";
 
+const Chat = () => {
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <button onClick={() => dispatch(clearMessages())} style={{ margin: "10px", padding: "8px", backgroundColor: "red", color: "white", border: "none", cursor: "pointer" }}>
+                Очистить историю
+            </button>
+        </div>
+    );
+};
+
+export default Chat;
