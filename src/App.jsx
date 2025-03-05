0efe417c9963a6d52db1407681949f806ea3b5e1
lekanava <lekanava8@gmail.com>
2025-03-05 18:48:26 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import { Provider } from 'react-redux'
-import { store } from './store/store'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { clearMessages } from './store/messageSlice'  // Импортируем действие для очистки сообщений
+import { Provider } from 'react-redux' import { store } from './store/store' import Header from './components/Header' import Footer from './components/Footer' import { Outlet } from 'react-router-dom'
 
 function App() {
-  const dispatch = useDispatch()
 
-  // Функция для очистки сообщений
-  const handleClearMessages = () => {
-    dispatch(clearMessages())
-  }
+return (
+	<>
+		<div className='font-quicksand'>
+			<Provider store={store} >
+				<Header />
+				<Outlet />
+				<Footer />
+			</Provider>
+		</div>
+	</>
+)
 
-  return (
-    <div className='font-quicksand'>
-      <Provider store={store}>
-        <Header />
-        
-        {/* Кнопка очистки сообщений */}
-        <div>
-          <button
-            onClick={handleClearMessages}
-            style={{
-              margin: '10px',
-              padding: '8px',
-              backgroundColor: 'red',
-              color: 'white',
-              border: 'none',
-              cursor: 'pointer',
-            }}
-          >
-            Очистить всё
-          </button>
-        </div>
-
-        {/* Контент страницы */}
-        <Outlet />
-        <Footer />
-      </Provider>
-    </div>
-  )
 }
 
-export default App
+export default
